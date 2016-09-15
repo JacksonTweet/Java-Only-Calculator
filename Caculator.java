62e69ad6cec853bccefcc48fac6ff0dedb3d1a22
Jackson Tweet <jacksontweet@createninnovate.com>
2016-09-15 13:32:24 -0500

    Update and rename Caculator.class to Caculator.java
    Starting important work on input :D

---
+++ Caculator.java
@@ -0,0 +1,28 @@
+//packaging stuff
+package Calculator;
+
+//importing stuff
+import java.util.Scanner;  
+ 
+class Caculator  
+ {  
+  public static void main(String args[])  
+   {
+    //Start commands
+    int a;  
+    float b;  
+    String s;  
+    
+    //start input area
+    Scanner in = new Scanner(System.in);  
+    System.out.println("Enter a command: (Dont know the commands? Type "Help" Or go to goo.gl/7baiQK)");  
+    s = in.nextLine();  
+    System.out.println("You entered string "+s);  
+    System.out.println("Enter an integer");  
+    a = in.nextInt();  
+    System.out.println("You entered integer "+a);  
+    System.out.println("Enter a float");  
+    b = in.nextFloat();  
+    System.out.println("You entered float "+b);   
+  }  
+ }  
