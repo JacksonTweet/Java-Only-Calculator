0a5fc73fc05e71c7a0511bce1ff61c353742e15a
Jackson Tweet <jacksontweet@createninnovate.com>
2016-09-16 14:36:18 -0500

    Update Caculator.java

--- Caculator.java
+++ Caculator.java
@@ -9,9 +9,9 @@ class Caculator
   public static void main(String args[])  
    {
     //Start commands
-    String error = 'Error! Command not found! type help for cammands or check the wiki';
-    String test = 'test dis';  
-    String help = 'help';  
+    String error = "Error! Command not found! type help for cammands or check the wiki";
+    String test = "test dis";  
+    String help = "help";  
     
     //input sections
     String input;  
@@ -37,14 +37,16 @@ class Caculator
     System.out.println("Enter a command: (Dont know the commands? Type 'help' Or go to goo.gl/7baiQK)");  
     input = in.nextLine();  
     
-    if (input = help) {
-        output = 'help';
-    } else if (input = test) {
-        output = 'test';
+    if (input.equals("help")) {
+        output = help;
+    } else if (input.equals("test")) {
+        output = test;
     } else {
-        output = 'error';
+        output = error;
     }
-        
+    System.out.println();
     System.out.println(""+output);
+    
   }  
  }  
+
